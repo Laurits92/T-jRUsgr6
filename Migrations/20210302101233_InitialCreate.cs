54643f21e507477e9bbf675200fa51f58650a662
Ibsylong <luanbanguyen94@gmail.com>
2021-03-02 11:13:06 +0100

    Database lavet!!!!!!!111!!!

---
+++ Migrations/20210302101233_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TøjRUsgr6.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clothes",
+                columns: table => new
+                {
+                    ClothingID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Category = table.Column<string>(type: "TEXT", nullable: true),
+                    Type = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Size = table.Column<string>(type: "TEXT", nullable: true),
+                    Color = table.Column<string>(type: "TEXT", nullable: true),
+                    Prize = table.Column<double>(type: "REAL", nullable: false),
+                    Image = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clothes", x => x.ClothingID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clothes");
+        }
+    }
+}
---
+++ Models/Clothing.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TøjRUsgr6.Models
+{
+    public class Clothing
+    {
+        public int ClothingID { get; set; }
+        public string Title { get; set; }
+
+        public string Category { get; set; }
+
+        public  string Type { get; set; }
+
+        public string Description { get; set; }
+
+        public string Size { get; set; }
+
+        public string Color { get; set; }
+
+        public double Prize { get; set; }
+
+        public string Image { get; set; }
+    }
+}
